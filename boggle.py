42d3a6769c1c8c8c9060ea0b3a08d797fa4547b2
= <jwoestman@gmail.com>
2020-03-29 08:10:20 -0500

    First working version of boggle board!

--- boggle.py
+++ boggle.py
@@ -1,21 +1,8 @@
 def boggle(board, word):
-    # first do a sanity check to determine all the letters exist in the board, in the right quantity
-    allLetters = [letter for row in board for letter in row]
-    for letter in word:
-        if letter in allLetters:
-            allLetters.remove(letter)
-        else:
-            return False
-
-    letters = []
-    for rowNum in range(len(board)):
-        for colNum in range(len(board)):
-            #tuple with rowCoordinate, columnCoordinate, letter
-            letters.append((rowNum, colNum, board[rowNum][colNum]))
-
+    # list to keep track of what letters we have used in our search
     usedLetters = []
 
-    def isOutOfBounds (spot):
+    def is_out_of_bounds(spot):
         if spot[0] < 0 or spot[0] >= len(board):
             return True
         elif spot[1] < 0 or spot[1] >= len(board):
@@ -23,9 +10,8 @@ def boggle(board, word):
         else:
             return False
 
-    def isAdjacent (one, two):
-        #check out of bounds
-        if isOutOfBounds(one) or isOutOfBounds(two):
+    def is_adjacent(one, two):
+        if is_out_of_bounds(one) or is_out_of_bounds(two):
             return False
 
         if one[0] == two[0] and one[1] == two[1]:
@@ -33,41 +19,58 @@ def boggle(board, word):
 
         return abs(one[0] - two[0]) in [0, 1] and abs(one[1] - two[1]) in [0, 1]
 
+    def get_adjacent(l):
+        return [spot for spot in letters if is_adjacent(l, spot) and spot not in usedLetters]
 
-    def getAdjacent (l):
-        return [spot for spot in letters if isAdjacent(l, spot)]
+    # begin the recursive fun!
+    def get_next_letter(letter, depth):
+        adjacentLetters = [adj for adj in get_adjacent(letter) if word[depth] == adj[2]]
 
-    #get all the spots with the first letter of the word
-    startingLetters = [starts for starts in letters if starts[2] == word[0]]
+        #stop recursing if we are on the last letter, and we found adjacent matches
+        if depth == len(word) - 1 and len(adjacentLetters) > 0:
+            # we did it!
+            usedLetters.append(adjacentLetters[0])
+            return True
 
-    #begin the recursive fun!
-    def recurseOnLetter(letter, depth):
-        adjacentLetters = [adj for adj in getAdjacent(letter) if word[depth] == adj[2]]
-        print('depth', depth, letters, adjacentLetters)
         if (len(adjacentLetters) == 0):
             return False
 
         for adjacentLetter in adjacentLetters:
-            recurseOnLetter(adjacentLetter, depth + 1)
+            usedLetters.append(adjacentLetter)
+            if get_next_letter(adjacentLetter, depth + 1):
+                # we did it!
+                return True
+            else:
+                usedLetters.pop()
+
+    # collapse the board to a single list
+    letters = []
+    for rowNum in range(len(board)):
+        for colNum in range(len(board)):
+            # tuple with rowCoordinate, columnCoordinate, letter
+            letters.append((rowNum, colNum, board[rowNum][colNum]))
+
+    # get all the spots with the first letter of the word
+    startingLetters = [starts for starts in letters if starts[2] == word[0]]
 
     for startingLetter in startingLetters:
-        #mark this letter as used
+        # mark this letter as used
         usedLetters.append(startingLetter)
-        if recurseOnLetter(startingLetter, 1):
-            #we did it!
+        if get_next_letter(startingLetter, 1):
+            # we did it!
+            print(usedLetters)
             return True
         else:
-            #this letter did't work out
+            # this letter did't work out
             usedLetters = []
 
-
-
-
+    # welp, didn't find anything
+    return False
 
 
 theBoard = \
     [["I", "L", "A", "W"],
      ["B", "N", "G", "E"],
-     ["I", "U", "A", "O"],
-     ["A", "S", "R", "L"]]
-boggle(theBoard, 'BINGO')
+     ["I", "U", "A", "S"],
+     ["N", "G", "O", "O"]]
+print(boggle(theBoard, 'BINGO'))
